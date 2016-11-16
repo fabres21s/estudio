b98fc88cdbb6b2db9c4688cb747a58d94b1c4d29
fabres21s <fabres21s@gmail.com>
2016-11-16 06:44:37 -0500

    Login con JSF

---
+++ jsf/jsf_login/src/com/journaldev/jsf/dao/LoginDAO.java
@@ -0,0 +1,14 @@
+package com.journaldev.jsf.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class LoginDAO {
+
+	public static boolean validate(String user, String password) {
+		return true;
+	}
+}
