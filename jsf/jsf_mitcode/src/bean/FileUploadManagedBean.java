fee571a8db6fb946a4a8c7697c9ddd0e7938b492
fabres21s <fabres21s@gmail.com>
2016-12-27 18:09:57 -0500

    pruebas con el upload

--- jsf/jsf_mitcode/src/bean/FileUploadManagedBean.java
+++ jsf/jsf_mitcode/src/bean/FileUploadManagedBean.java
@@ -1,5 +1,9 @@
 package bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -9,8 +13,17 @@ import org.primefaces.model.UploadedFile;
 @ManagedBean
 @SessionScoped
 public class FileUploadManagedBean {
-	UploadedFile file;
+	private UploadedFile file;
+	private List<UploadedFile> files = new ArrayList<UploadedFile>();
+	
+	@PostConstruct
+	public void init() {
+	}
+	
+	
 
+	
+	
 	public UploadedFile getFile() {
 		return file;
 	}
@@ -19,10 +32,28 @@ public class FileUploadManagedBean {
 		this.file = file;
 	}
 
+	public void subir() {
+		
+	}
+	public void delete(UploadedFile file) {
+		files.remove(file);
+	}
+	
 	public void fileUploadListener(FileUploadEvent e){
 		// Get uploaded file from the FileUploadEvent
 		this.file = e.getFile();
 		// Print out the information of the file
 		System.out.println("Uploaded File Name Is :: "+file.getFileName()+" :: Uploaded File Size :: "+file.getSize());
+		files.add(file);
+		System.out.println("FileUploadManagedBean.fileUploadListener() ::: "+files.size());
+	}
+
+	public List<UploadedFile> getFiles() {
+		System.out.println("FileUploadManagedBean.getFiles() ::: "+files.size());
+		return files;
+	}
+
+	public void setFiles(List<UploadedFile> files) {
+		this.files = files;
 	}
 }
